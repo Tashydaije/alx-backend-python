23b26d2aea5665ecda320cfc1af83fee4fd6aa74
Tashydaije <mbuguanatasha01@gmail.com>
2024-06-11 14:07:39 +0000

    fix, use Generator for type hinting

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,23 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """ coroutine func that yields a random number between 0 and 10"""
+async def async_generator() -> Generator[float, None, None]:
+    """Loops 10 times and yields a random number in [0..10]
+
+        In each loop, the generator waits for 1 second then
+        yields a random number between 0 and 10
+
+        Parameters
+        ----------
+        None
+
+        Yields
+        ------
+        Random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
